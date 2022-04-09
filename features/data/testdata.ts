a371f9201d9accecff3e888ad994e27d218dbdba
NOORMOHIDEEN <81789614+NOORMOHIDEEN@users.noreply.github.com>
2022-04-09 15:43:53 +0530

    Add files via upload

---
+++ features/data/testdata.ts
@@ -0,0 +1,24 @@
+const data = {
+  AppURL: {
+    URL: "https://spring-petclinic-community.herokuapp.com/"
+  },
+  TestData: {
+    OwnerDetails: {
+      firstName: "SAM",
+      lastName: "VICTOR",
+      address: "test",
+      city: "chennai",
+      telephone: "98745"
+    },
+    PetDetails: {
+      petName: "Chukoo",
+      dob: "2021-08-17"
+    },
+    ExpectedPetTypes: ["bird", "cat", "dog", "hamster", "lizard", "snake"],
+     ActualPetDetails: []  
+  }
+};
+export default data;
+
+
+
